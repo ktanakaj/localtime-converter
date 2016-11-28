80c0577999cf488018f357007ee661138a262113
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2016-11-28 23:14:39 +0900

    ビルドツールをwebpackに変更しビルド時にminifyを実施&momentをCDNから読み込むよう変更, バージョン更新

---
+++ localtime-converter/webpack.config.js
@@ -0,0 +1,25 @@
+﻿/**
+ * @file webpack設定スクリプト。
+ */
+module.exports = {
+    entry: './src/app/main.ts',
+	output: {
+		path: __dirname + '/src',
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ["", ".ts", ".tsx", ".js"]
+	},
+	module: {
+		loaders: [
+			{ test: /\.tsx?$/, loader: "ts-loader" }
+		]
+	},
+	externals: {
+		"moment": "moment",
+		"moment-timezone": "moment",
+	},
+	watchOptions: {
+		poll: 1000
+	}
+};
