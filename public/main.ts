509619cf3314f5d1b5d03b16e90636bbffcfe5b5
Koichi Tanaka <ktanakaj@users.noreply.github.com>
2018-05-25 09:23:56 +0900

    CDNのJSもバージョン更新

--- public/main.ts
+++ public/main.ts
@@ -2,6 +2,9 @@
  * @file ローカル日時変換ブートローダー。
  * @author Koichi Tanaka
  */
+// ※ ↓は含めなくてもコンパイルは通るので軽量化のために外す。代わりにindex.htmlにCDNから読み込む設定を入れている。
+// import "zone.js";
+// import 'reflect-metadata';
 import { enableProdMode } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeJa from '@angular/common/locales/ja';
